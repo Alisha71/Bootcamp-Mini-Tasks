435c10f7cb3dedd8df14c66db7a7f2dad9925991
Alisha71 <alisha.shah71@hotmail.com>
2025-04-15 14:06:12 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,23 @@
+# declare the variables with the values given
+# convert num1 (a float) to an integer and store it.
+# convert num2 (an integer) to a float and store it.
+# convert num3 (an integer) to a string and store it.
+# convert string1 (a string) to an integer and store it. 
+
+# declare the variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# convert the variables according to the task
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+# print out each variable on a seperate line
+print(num1)
+print(num2)
+print(num3)
+print(string1)
